65f621db6b2612ec10c48ff475501e090b1d05ce
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-02-05 15:48:10 +0530

    Completed Functions in Depth

--- Functions/index.ts
+++ Functions/index.ts
@@ -1,5 +1,30 @@
-function sum(a: number, b: number): number {
-  return a + b;
-}
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
 
-console.log(sum(5, 10));
+// console.log(sum(5, 10));
+
+// const greet = (name: string, id: number):string => {
+//   return `Welcome, ${name} and your id is ${id}`;
+// }
+
+// Default Parameter
+
+// const greet = (name: string, id: number = 1) => {
+//   return `Welcome, ${name} and ur id is ${id}`;
+// };
+
+// Optional Parameter
+
+const greet = (name: string, id?: number) => {
+  if (id) {
+    return `Welcome, ${name} and ur id is ${id}`;
+  } else {
+    return `Welcome, ${name}`;
+  }
+};
+
+const emp = greet("Vinod");
+const emp2 = greet("Vinod", 2);
+console.log(emp);
+console.log(emp2);
---
+++ Type Inferences/index.ts
@@ -0,0 +1,2 @@
+let myNum = '44' 
+// myNum = 5 //Throws an error as the type of myNum is string as Inferred by TypeScript
