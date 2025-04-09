27bec9ede11223990644eeac6c93c78dd63f49fb
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2025-04-09 20:18:03 +0530

    Learned Access Modifiers

--- OOPS/index.ts
+++ OOPS/index.ts
@@ -8,26 +8,38 @@
 // }
 
 class Person {
-  name: string;
-  age: number;
-  hobbies: string[];
-  constructor(name: string, age: number, hobbies: string[]) {
+  // public name: string;
+  // protected age: number;
+  // private hobbies: string[];
+
+  // Short Hand Property
+  constructor(
+    public name: string,
+    protected age: number,
+    private hobbies: string[]
+  ) {
     this.name = name;
     this.age = age;
     this.hobbies = hobbies;
   }
-  intro(): string {
-    return `Hi, my name is ${this.name}`;
+  introParent(): string {
+    return `Hi, my name is ${this.name} with hobbies ${this.hobbies}`;
   }
 }
 
 class Student extends Person {
-  grade: number;
-  constructor(name: string, age: number, hobbies: string[], grade: number) {
+  // grade: number;
+  constructor(
+    name: string,
+    age: number,
+    hobbies: string[],
+    public grade: number
+  ) {
     super(name, age, hobbies);
     this.grade = grade;
   }
   intro(): string {
+    // return `Hi, my name is ${this.name} and my marks are ${this.grade} while my hobbies are ${this.hobbies}`; // this will throw an error as hobbies is private
     return `Hi, my name is ${this.name} and my marks are ${this.grade}`;
   }
 }
@@ -35,3 +47,5 @@ class Student extends Person {
 const person1: Student = new Student("SK", 21, ["coding", "reading"], 95);
 
 console.log(person1.intro());
+
+// console.log(person1.hobbies); // will throw an error
